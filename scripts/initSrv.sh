fb53a6a8a8857a14c3b7ca05c76d1790ac2e3268
myownsoul <ericzhangyang@hotmail.com>
2017-11-17 11:33:31 +0800

    add docker tcp configuration into initSrv.sh

--- scripts/initSrv.sh
+++ scripts/initSrv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Basic Configuration
+echo "127.0.0.1 registry.devops.local" >> /etc/hosts
+
 # Install JDK
 java -version
 if [ $? -eq 0 ]
@@ -37,7 +40,7 @@ else
                 https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
         yum install -y docker-ce
         systemctl start docker
-        sed -i "s|ExecStart=/usr/bin/dockerd|ExecStart=/usr/bin/dockerd --registry-mirror=https://pee6w651.mirror.aliyuncs.com|g" /usr/lib/systemd/system/docker.service
+        sed -i "s|ExecStart=/usr/bin/dockerd|ExecStart=/usr/bin/dockerd -H unix:///var/run/docker.sock -H 0.0.0.0:2375  --insecure-registry=registry.devops.local:5000 --registry-mirror=https://pee6w651.mirror.aliyuncs.com|g" /usr/lib/systemd/system/docker.service
         systemctl daemon-reload
         systemctl restart docker
         systemctl enable docker
@@ -58,7 +61,7 @@ docker run -d \
 # Jenkins
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-yum install jenkins
+yum install -y jenkins
 systemctl enable jenkins
 systemctl start jenkins
 
